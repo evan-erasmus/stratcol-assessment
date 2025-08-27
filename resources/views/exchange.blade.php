3f868bb04ac8d2c8bf86ce18329251ce83b8bc9c
Evan Erasmus <evanerasmus14@gmail.com>
2025-08-27 22:00:08 +0200

    format currencies correctly

--- resources/views/exchange.blade.php
+++ resources/views/exchange.blade.php
@@ -12,7 +12,7 @@
                 <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700 gap-4 flex flex-col items-center justify-center">
                     <div class="text-md font-semibold text-gray-800 dark:text-gray-200">{{ $currency['code'] }} ({{ $currency['symbol'] }})</div>
                     <img src="https://flagcdn.com/{{ $currency['country_code_short'] }}.svg" alt="{{ $currency['code'] }}  Flag" class="w-24"/>
-                    <div class="text-xl font-semibold text-gray-800 dark:text-gray-200">R{{ 1.0 / $currency['exchange_rate'] }}</div>
+                    <div class="text-xl font-semibold text-gray-800 dark:text-gray-200">R{{ number_format(1.0 / $currency['exchange_rate'], 2) }}</div>
                 </div>
             @endforeach
         </div>
